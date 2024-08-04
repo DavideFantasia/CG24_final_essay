a120d587f593af8388d8be09c30ca0377250d6cd
Davide Fantasia <davide.fantasia04@gmail.com>
2024-08-04 20:19:39 +0200

    model importer
    gltf/glb model importer, correct textures, correct and lighter model

--- progetto_finale/Main.cpp
+++ progetto_finale/Main.cpp
@@ -30,6 +30,9 @@
 #include "material.h"
 #include "terrainGenerator.h"
 #include "lights.h"
+#include "obj_loader.h"
+
+#include "..\common\gltf_loader.h"
 
 
 /*
@@ -76,7 +79,7 @@ glm::mat4 proj;
 glm::mat4 view;
 
 /* program shaders used */
-shader heightmap_shader, skybox_shader, fsq_shader, depth_shader;
+shader lighting_shader, skybox_shader, fsq_shader, uv_shader, depth_shader;
 float heightmapScale = 0.25f; //scale of the dune height (heightmap scale value)
 float heightmapRep = 4.f; //repetition of the heightmap on the heightmap
 
@@ -86,6 +89,9 @@ renderable r_terrain;
 renderable r_cube;
 // quad da renderizzare a schermo intero a scopo di debug per le texture
 renderable r_quad;
+
+renderable lamp_r, bust_r;
+glm::mat4 lamp_model_mat, bust_model_mat;
 
 void draw_large_cube() {
 	r_cube = shape_maker::cube();
@@ -119,26 +125,78 @@ void window_size_callback(GLFWwindow* window, int _width, int _height)
 	glViewport(0, 0, width, height);
 	proj = glm::perspective(glm::radians(40.f), width / float(height), 2.f, 20.f);
 
-	glUseProgram(heightmap_shader.program);
-	glUniformMatrix4fv(heightmap_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
-	glUniformMatrix4fv(heightmap_shader["uView"], 1, GL_FALSE, &view[0][0]);
+	glUseProgram(lighting_shader.program);
+	glUniformMatrix4fv(lighting_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
+	glUniformMatrix4fv(lighting_shader["uView"], 1, GL_FALSE, &view[0][0]);
 	glUseProgram(0);
 
 }
-/*------- terrain texture ----------*/
+/*---------------- texture ----------------*/
+//texture per il terreno
 texture heightmap, sandTexture;
 char heightmap_name[256] = { "./textures/terrain/height_map_blurred.png" };
 char sandTexture_name[256] = { "./textures/terrain/sand_texture.jpg" };
-/*-------- skybox texture ----------*/
+//texture del busto del david
+texture diffuse_david, roughness_david, metallic_david, normal_david, ao_david, uv_david;
+texture diffuse_lamp, roughness_lamp, metallic_lamp, normal_lamp, ao_lamp;
+//texture per lo skybox
 texture daySkybox, nightSkybox;
+
 /*
 * id 0: heightmap terrain
 * id 1: desert texture terrain
 * id 2: daySkybox
 * id 3: nightSkybox
-* id 4: shadowmap del sole, con scopo di shadow mapping
-* id 5: shadowmap dello spot light, con scopo di shadow mapping
+* 
+* id 4: david_diffuse
+* id 5: david_roughness
+* id 6: daivd_metallic
+* id 7: david_normal
+* id 8: david_ao
+* id 9: david_uv
+* 
+* id 10: lamp diffuse
+* id 11: lamp roughness
+* id 12: lamp metallic
+* id 13: lamp normal
+* id 14: lamp ao
+
+* ------
+* id 20: shadowmap del sole, con scopo di shadow mapping
+* id 21: shadowmap dello spot light, con scopo di shadow mapping
 */
+void load_textures() {
+	heightmap.load(std::string(heightmap_name), 0, false);
+	sandTexture.load(std::string(sandTexture_name), 1, true);
+	
+	std::string path = "./textures/cube_map/day/";
+	daySkybox.load_cubemap(path + "posx.bmp", path + "negx.bmp",
+		path + "posy.bmp", path + "negy.bmp",
+		path + "posz.bmp", path + "negz.bmp", 2);
+
+	path = "./textures/cube_map/night/";
+	nightSkybox.load_cubemap(path + "posx.png", path + "negx.png",
+		path + "posy.png", path + "negy.png",
+		path + "posz.png", path + "negz.png", 3);
+	//texture del busto
+	path = "./models/bust/textures/";
+	diffuse_david.load(path + "diffuse.png",4,true);
+	roughness_david.load(path + "roughness.png", 5, false);
+	metallic_david.load(path + "metallic.png", 6, false);
+	normal_david.load(path + "normal_tangent.png", 7, false);
+	ao_david.load(path + "ao.png", 8, false);
+
+	//texture del lampione
+	path = "./models/lamp/textures/";
+	
+
+	diffuse_lamp.load(path + "diffuse.png", 10, true);
+	roughness_lamp.load(path + "roughness.png", 11, false);
+	metallic_lamp.load(path + "metallic.png", 12, false);
+	normal_lamp.load(path + "normal_tangent.png", 13, false);
+	ao_lamp.load(path + "ambient_occlusion.png", 14, false);
+}
+
 /* ---- framebuffer object for shadowmapping ----*/
 frame_buffer_object sunDepthBuffer, lampDepthBuffer;
 
@@ -165,23 +223,8 @@ struct projector {
 };
 projector sunProjector, lampProjector;
 
-
-void load_textures() {
-	heightmap.load(std::string(heightmap_name), 0, false);
-	sandTexture.load(std::string(sandTexture_name), 1, true);
-	
-	std::string path = "./textures/cube_map/day/";
-	daySkybox.load_cubemap(path + "posx.bmp", path + "negx.bmp",
-		path + "posy.bmp", path + "negy.bmp",
-		path + "posz.bmp", path + "negz.bmp", 2);
-
-	path = "./textures/cube_map/night/";
-	nightSkybox.load_cubemap(path + "posx.png", path + "negx.png",
-		path + "posy.png", path + "negy.png",
-		path + "posz.png", path + "negz.png", 3);
-}
-
 void RenderScene(shader shader);
+void render_gltf(shader shader, std::vector<renderable> obj, box3 bbox);
 
 int main(void)
 {
@@ -193,6 +236,7 @@ int main(void)
 
 	width = 1000;
 	height = 800;
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(width, height, "Desolazione - Davide Fantasia's Project", NULL, NULL);
 
@@ -212,6 +256,8 @@ int main(void)
 
 	glewInit();
 
+	glEnable(GL_MULTISAMPLE);
+
 	printout_opengl_glsl_info();
 
 	/*impostazione delle callback per l'input del mouse*/
@@ -221,21 +267,21 @@ int main(void)
 
 	/* load the shaders */
 	std::string shaders_path = "./shaders/";
-	heightmap_shader.create_program((shaders_path + "pbr.vert").c_str(), (shaders_path + "pbr.frag").c_str());
+	lighting_shader.create_program((shaders_path + "pbr.vert").c_str(), (shaders_path + "pbr.frag").c_str());
 	skybox_shader.create_program((shaders_path + "skybox.vert").c_str(), (shaders_path + "skybox.frag").c_str());
 	fsq_shader.create_program((shaders_path + "fsq.vert").c_str(), (shaders_path + "fsq.frag").c_str());
 	depth_shader.create_program((shaders_path + "depthShader.vert").c_str(), (shaders_path + "depthShader.frag").c_str());
 
-	validate_shader_program(heightmap_shader.program);
+	validate_shader_program(lighting_shader.program);
 	validate_shader_program(skybox_shader.program);
 	validate_shader_program(fsq_shader.program);
 	validate_shader_program(depth_shader.program);
 
 	/* Set the uT matrix to Identity */
-	glUseProgram(heightmap_shader.program);
-	glUniformMatrix4fv(heightmap_shader["uModel"], 1, GL_FALSE, &glm::mat4(1.0)[0][0]);
-	glUniform1f(heightmap_shader["uHeightScale"], heightmapScale); // Imposta il valore desiderato per la scala delle dune
-	glUniform1f(heightmap_shader["uTextureRep"], heightmapRep); // Imposta il numero di ripetizioni della texture
+	glUseProgram(lighting_shader.program);
+	glUniformMatrix4fv(lighting_shader["uModel"], 1, GL_FALSE, &glm::mat4(1.0)[0][0]);
+	glUniform1f(lighting_shader["uHeightScale"], heightmapScale); // Imposta il valore desiderato per la scala delle dune
+	glUniform1f(lighting_shader["uTextureRep"], heightmapRep); // Imposta il numero di ripetizioni della texture
 	glUseProgram(0);
 
 	check_gl_errors(__LINE__, __FILE__);
@@ -244,8 +290,28 @@ int main(void)
 	shape s_plane;
 	std::thread terraingeneration_thread(TerrainGenerator, &s_plane);
 
+
+	// --------------------------------------------------------------------------------------------------------------
+	// load an obj object
+	//loadOBJ("./models/bust/bust.obj", bust_r);
+
+	loadOBJ("./models/lamp/street_light.obj", lamp_r);
+	check_gl_errors(__LINE__, __FILE__, true);
+
+	gltf_loader gltfLoader;
+	box3 bbox;
+	std::vector <renderable> obj;
+
+	gltfLoader.load_to_renderable("./models/bust/bust_v3.glb", obj , bbox);
+
+	//matrice di modello del lampione
+	lamp_model_mat = glm::scale(glm::mat4(1.f), glm::vec3(0.25f, 0.25f, 0.25f));
+	//matrice di modello del busto
+	bust_model_mat = glm::scale(glm::mat4(1.f), glm::vec3(0.005f, 0.005f, 0.005f));
 	//matrice di modello del terreno
-	glm::mat4 model_matrix = glm::scale(glm::mat4(1.f), glm::vec3(3.f, 1.f, 3.f));
+	glm::mat4 terrain_model_mat = glm::scale(glm::mat4(1.f), glm::vec3(3.f, 1.f, 3.f));
+
+	// --------------------------------------------------------------------------------------------------------------
 
 	/* Transformation to setup the point of view on the scene */
 	proj = glm::perspective(glm::radians(40.f), width / float(height), 1.f, 10.f);
@@ -263,27 +329,29 @@ int main(void)
 	distance_light = 5;
 
 	/* ----------- Passaggio Uniform per la creazione del Terrain ----------------*/
-	glUseProgram(heightmap_shader.program);
-	glUniformMatrix4fv(heightmap_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
-	glUniformMatrix4fv(heightmap_shader["uView"], 1, GL_FALSE, &view[0][0]);
+	glUseProgram(lighting_shader.program);
+	glUniformMatrix4fv(lighting_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
+	glUniformMatrix4fv(lighting_shader["uView"], 1, GL_FALSE, &view[0][0]);
 
 	/*-------- Passaggio ID delle texture alla Shader ----------*/
-	glUniform1i(heightmap_shader["uColorImage"], 0);
+	glUniform1i(lighting_shader["uColorImage"], 0);
 
 
 	/* ---------------- creazione e passaggio informazioni sulle luci --------------------*/
 	Light sun = sun.directional_init(glm::vec3(0.f, -1.f, 0.f));
-	sun.set_uniform(heightmap_shader.program);
+	sun.set_uniform(lighting_shader.program);
 
 
 	Light spotLight = spotLight.spotlight_init(glm::vec3(1.f, 2.f, 1.f), glm::vec3(-0.5f, -1.f, -1.f), 25.f, 35.f);
-	spotLight.set_uniform(heightmap_shader.program);
+	spotLight.set_uniform(lighting_shader.program);
 	
-	glUniform3fv(heightmap_shader["uViewPos"], 1, &camera.Position[0]);
-	glUniform1f(heightmap_shader["uBias"], depth_bias);
+	glUniform3fv(lighting_shader["uViewPos"], 1, &camera.Position[0]);
+	glUniform1f(lighting_shader["uBias"], depth_bias);
+	
+	check_gl_errors(__LINE__, __FILE__, true);
 	/*
 	glUseProgram(0);
-	check_gl_errors(__LINE__, __FILE__, true);
+	
 	*/
 
 	/* -------- Passaggio Uniform alla Texture Shader ------------*/
@@ -311,9 +379,16 @@ int main(void)
 	renderable r_cube = shape_maker::cube();
 
 	SandTerrainMaterial sand_material; //materiale del terreno
+	BustMaterial bust_material;
+	LampMaterial lamp_material;
+	//cube_material.init(diffuse_cube.id,roughness_cube.id, metallic_cube.id, normal_cube.id);
+	
+	bust_material.init(diffuse_david.tu,roughness_david.tu,metallic_david.tu,normal_david.tu,ao_david.tu);
+	lamp_material.init(diffuse_lamp.tu,roughness_lamp.tu,metallic_lamp.tu,normal_lamp.tu,ao_lamp.tu);
 
-	/*---- attesa fine generazione del terreno -----*/
+	/*---- attesa fine generazione del terreno e caricamento modello -----*/
 	terraingeneration_thread.join();
+
 	s_plane.to_renderable(r_terrain);
 	r_quad = shape_maker::quad();
 
@@ -331,8 +406,6 @@ int main(void)
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		
-		// Calcolare l'angolo di rotazione del sole
 		
 		// Calcolare l'angolo di rotazione del sole
 		dayNight_rotation_angle += (dayNight_rotation_speed * deltaTime);
@@ -353,19 +426,17 @@ int main(void)
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 			glCullFace(GL_FRONT); //per sistemare il peter-panning
 			// -------- render scene
-			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &model_matrix[0][0]);
+			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &terrain_model_mat[0][0]);
 			r_terrain.bind();
 			glDrawElements(r_terrain().mode, r_terrain().count, r_terrain().itype, 0);
-
-
-			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &(glm::scale(glm::mat4(1.f), glm::vec3(0.5f, 0.5f, 0.5f)))[0][0]);
-			r_cube.bind();
-			glDrawElements(r_cube().mode, r_cube().count, r_cube().itype, 0);
+			check_gl_errors(__LINE__, __FILE__, true);
+			//RenderScene(depth_shader);
+			render_gltf(depth_shader, obj, bbox);
 			// ------
 			glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glUseProgram(0);
-
+		/*
 		//rendering depth map del faretto
 		glUseProgram(depth_shader.program);
 
@@ -377,71 +448,67 @@ int main(void)
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 			glCullFace(GL_FRONT); //per sistemare il peter-panning
 			// -------- render scene
-			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &model_matrix[0][0]);
+			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &terrain_model_mat[0][0]);
 			r_terrain.bind();
 			glDrawElements(r_terrain().mode, r_terrain().count, r_terrain().itype, 0);
-
-
-			glUniformMatrix4fv(depth_shader["uModel"], 1, GL_FALSE, &(glm::scale(glm::mat4(1.f), glm::vec3(0.5f, 0.5f, 0.5f)))[0][0]);
-			r_cube.bind();
-			glDrawElements(r_cube().mode, r_cube().count, r_cube().itype, 0);
+			render_gltf(depth_shader, obj, bbox);
 		// ------
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glUseProgram(0);
 		// ripristino del frame buffer attivo
-		
+		*/
 
 		/*-----------------------------------------------------------------------------*/
 		
 		glViewport(0, 0, width, height);
 
-		
 		// Input
 		processInput(window);
 		view = camera.GetViewMatrix();
 
 		//Terrain Rendering
-		glUseProgram(heightmap_shader.program);
-		sun.set_uniform(heightmap_shader.program); //aggiornamento dei dati del sole che ruota
+		glUseProgram(lighting_shader.program);
+		sun.set_uniform(lighting_shader.program); //aggiornamento dei dati del sole che ruota
 
-		glUniformMatrix4fv(heightmap_shader["uView"], 1, GL_FALSE, &view[0][0]);
-		glUniformMatrix4fv(heightmap_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
-		glUniformMatrix4fv(heightmap_shader["uModel"], 1, GL_FALSE, &model_matrix[0][0]);
-		glUniform3fv(heightmap_shader["uViewPos"], 1, &camera.Position[0]);
+		glUniformMatrix4fv(lighting_shader["uView"], 1, GL_FALSE, &view[0][0]);
+		glUniformMatrix4fv(lighting_shader["uProj"], 1, GL_FALSE, &proj[0][0]);
+		glUniformMatrix4fv(lighting_shader["uModel"], 1, GL_FALSE, &terrain_model_mat[0][0]);
+		glUniform3fv(lighting_shader["uViewPos"], 1, &camera.Position[0]);
 
 		//shadowmapping del sole
-		glUniformMatrix4fv(heightmap_shader["uSunLightSpaceMatrix"], 1, GL_FALSE, &(sunProjector.light_matrix())[0][0]);
-		//passaggio della shadowmap del sole(slot 4)
+		glUniformMatrix4fv(lighting_shader["uSunLightSpaceMatrix"], 1, GL_FALSE, &(sunProjector.light_matrix())[0][0]);
+		//passaggio della shadowmap del sole(slot 20)
 		GLint at;
 		glGetIntegerv(GL_ACTIVE_TEXTURE, &at);
-		glActiveTexture(GL_TEXTURE4);
+		glActiveTexture(GL_TEXTURE20);
 		glBindTexture(GL_TEXTURE_2D, sunDepthBuffer.id_depth);
-		glUniform1i(glGetUniformLocation(heightmap_shader.program, "uSunShadowMap"), 4);
+		glUniform1i(glGetUniformLocation(lighting_shader.program, "uSunShadowMap"), 20);
 		glActiveTexture(at);
-
+		/*
 		//shadowmapping del faretto
-		glUniformMatrix4fv(heightmap_shader["uLampLightSpaceMatrix"], 1, GL_FALSE, &(lampProjector.light_matrix())[0][0]);
-		//passaggio della shadowmap del faretto(slot 5)
+		glUniformMatrix4fv(lighting_shader["uLampLightSpaceMatrix"], 1, GL_FALSE, &(lampProjector.light_matrix())[0][0]);
+		//passaggio della shadowmap del faretto(slot 21)
 		glGetIntegerv(GL_ACTIVE_TEXTURE, &at);
-		glActiveTexture(GL_TEXTURE5);
+		glActiveTexture(GL_TEXTURE21);
 		glBindTexture(GL_TEXTURE_2D, lampDepthBuffer.id_depth);
-		glUniform1i(glGetUniformLocation(heightmap_shader.program, "uLampShadowMap"), 5);
+		glUniform1i(glGetUniformLocation(lighting_shader.program, "uLampShadowMap"), 21);
 		glActiveTexture(at);
-		
+		check_gl_errors(__LINE__, __FILE__, true);
+		*/
 		//passaggio uniform del materiale
-		sand_material.set_shader_uniforms(heightmap_shader.program);
+		sand_material.set_shader_uniforms(lighting_shader.program);
+		check_gl_errors(__LINE__, __FILE__, true);
 		r_terrain.bind();
+		check_gl_errors(__LINE__, __FILE__, true);
 		glDrawElements(r_terrain().mode, r_terrain().count, r_terrain().itype, 0);
-		
-		//disegno del cubo
-		glUniformMatrix4fv(heightmap_shader["uModel"], 1, GL_FALSE, &(glm::scale(glm::mat4(1.f), glm::vec3(0.5f, 0.5f, 0.5f)))[0][0]);
-		
-		r_cube.bind();
-		glDrawElements(r_cube().mode, r_cube().count, r_cube().itype, 0);
-
 		check_gl_errors(__LINE__, __FILE__, true);
 		
+		//disegno del busto
+		bust_material.set_shader_uniforms(lighting_shader.program);
+		//lamp_material.set_shader_uniforms(lighting_shader.program);
+		render_gltf(lighting_shader, obj, bbox);
+
 		//skybox texture
 		glDepthFunc(GL_LEQUAL);
 		glUseProgram(skybox_shader.program);
@@ -461,10 +528,16 @@ int main(void)
 	return 0;
 }
 
-void RenderScene(shader shader) {
-
+void render_gltf(shader shader, std::vector<renderable> obj, box3 bbox) {
+	for (unsigned int i = 0; i < obj.size(); ++i) {
+		obj[i].bind();
+		// each object had its own transformation that was read in the gltf file
+		// glm::translate(bust_model_mat, glm::vec3(0.f, 3.f, 0.f)) * glm::translate(glm::rotate(obj[i].transform, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f)), glm::vec3(-bbox.center()))
+		// glm::translate(glm::rotate(bust_model_mat * obj[i].transform, glm::radians(20.f), glm::vec3(0.f, 0.f, 1.f)), glm::vec3(0.f, 5.f, 0.f))
+		glUniformMatrix4fv(shader["uModel"], 1, GL_FALSE, &(glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.35f, 0.f)) * glm::translate(glm::rotate(bust_model_mat*obj[i].transform, glm::radians(20.f), glm::vec3(0.f, 0.f, 1.f)), glm::vec3(-bbox.center())))[0][0]);
+		glDrawElements(obj[i]().mode, obj[i]().count, obj[i]().itype, 0);
+	}
 }
-
 
 /* --------------- callback per la gestione della camera ------------ */
 void processInput(GLFWwindow* window) {
--- progetto_finale/material.h
+++ progetto_finale/material.h
@@ -12,9 +12,12 @@ class Material {
 public:
     std::string name;
 
+    bool has_heightmap;
+
     glm::vec3 diffuse_factor;
     float metallic_factor;
     float roughness_factor;
+    float ao_factor;
 
     GLint diffuse_map;
     bool has_diffuse_map;
@@ -28,19 +31,20 @@ public:
     GLint normal_map;
     bool has_normal_map;
 
-    float ao;
+    GLint ao_map;
+    bool has_ao_map;
 
     Material(const std::string& material_name)
-        : name(material_name), diffuse_factor(1.0f), metallic_factor(0.5f),
-          roughness_factor(0.5f), diffuse_map(-1), has_diffuse_map(false), metallic_map(-1),
-          has_metallic_map(false), roughness_map(-1), has_roughness_map(false), normal_map(-1),
-          has_normal_map(false), ao(1.0f) {}
+        : name(material_name), has_heightmap(false), diffuse_factor(1.0f), metallic_factor(0.5f),
+        roughness_factor(0.5f), diffuse_map(-1), has_diffuse_map(false), metallic_map(-1),
+        has_metallic_map(false), roughness_map(-1), has_roughness_map(false), normal_map(-1),
+        has_normal_map(false), ao_factor(0.05f), ao_map(-1), has_ao_map(false) {}
 
     virtual void set_shader_uniforms(GLuint shader_program) const {
-        
-        glUniform1f(glGetUniformLocation(shader_program, "material.metallic_factor"), metallic_factor);
-        glUniform1f(glGetUniformLocation(shader_program, "material.roughness_factor"), roughness_factor);
-        glUniform1f(glGetUniformLocation(shader_program, "material.ao"), ao);
+        if (has_heightmap) {
+            glUniform1i(glGetUniformLocation(shader_program, "has_heightmap"), 1);
+        }
+
 
         if(has_diffuse_map){
             glUniform1i(glGetUniformLocation(shader_program, "material.diffuse_map"), diffuse_map);
@@ -51,25 +55,36 @@ public:
         }
 
         if (has_metallic_map) {
-            glUniform1i(glGetUniformLocation(shader_program, "material.has_metallic_map"), 1);
             glUniform1i(glGetUniformLocation(shader_program, "material.metallic_map"), metallic_map);
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_metallic_map"), 1);
         }else{
             glUniform1f(glGetUniformLocation(shader_program, "material.metallic_factor"), metallic_factor);
             glUniform1i(glGetUniformLocation(shader_program, "material.has_metallic_map"), 0);
         }
 
         if (has_roughness_map) {
-            glUniform1i(glGetUniformLocation(shader_program, "material.has_roughness_map"), 1);
             glUniform1i(glGetUniformLocation(shader_program, "material.roughness_map"), roughness_map);
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_roughness_map"), 1);
         }
         else {
             glUniform1f(glGetUniformLocation(shader_program, "material.roughness_factor"), roughness_factor);
             glUniform1i(glGetUniformLocation(shader_program, "material.has_roughness_map"), 0);
         }
+        
+        if (has_ao_map) {
+            glUniform1i(glGetUniformLocation(shader_program, "material.ao_map"), ao_map);
+            glUniform1f(glGetUniformLocation(shader_program, "material.ao_factor"), ao_factor);
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_ao_map"), 1);      
+        }else {
+            glUniform1f(glGetUniformLocation(shader_program, "material.ao_factor"), ao_factor);
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_ao_map"), 0);
+        }
 
         if (has_normal_map){
-            glUniform1i(glGetUniformLocation(shader_program, "material.has_normal_map"), 1);
             glUniform1i(glGetUniformLocation(shader_program, "material.normal_map"), normal_map);
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_normal_map"), 1);
+        }else{
+            glUniform1i(glGetUniformLocation(shader_program, "material.has_normal_map"), 0);
         }
     }
 
@@ -82,34 +97,79 @@ public:
         diffuse_factor = glm::vec3(0.94f, 0.80f, 0.49f);
         metallic_factor = 0.005;
         roughness_factor = 0.95f;
-        ao = 0.05f;
+        ao_factor = 0.05f;
 
-        diffuse_map = 1; // ID della diffuse map
         has_diffuse_map = true;
+        has_heightmap = true;
+        diffuse_map = 1; // ID della diffuse map
 
-        //passaggio info per il materiale
-        /*
-        glUniform1i(glGetUniformLocation(heightmap_shader.program, "material.diffuse_map"), 1);
-        glUniform1i(glGetUniformLocation(heightmap_shader.program, "material.has_diffuse_map"), 1);
-        glUniform3fv(glGetUniformLocation(heightmap_shader.program, "material.specular"), 1, glm::value_ptr(glm::vec3(0.94f, 0.80f, 0.49f)));
-        glUniform1i(glGetUniformLocation(heightmap_shader.program, "material.has_normal_map"), 0);
-        glUniform1f(glGetUniformLocation(heightmap_shader.program, "material.metallic"), 0.75);
-        glUniform1i(glGetUniformLocation(heightmap_shader.program, "material.has_metallic_map"), 0);
-        glUniform1f(glGetUniformLocation(heightmap_shader.program, "material.roughness"), 0.5f);
-        glUniform1i(glGetUniformLocation(heightmap_shader.program, "material.has_roughness_map"), 0);
-        glUniform1f(glGetUniformLocation(heightmap_shader.program, "material.ao"), 0.05f);*/
+        has_ao_map = false;
+        has_normal_map = false;
+        has_metallic_map = false;
+        has_roughness_map = false;
     }
 
     void set_shader_uniforms(GLuint shader_program) const override {
         Material::set_shader_uniforms(shader_program);
+    }
+};
+
+class BustMaterial : public Material {
+public:
+    BustMaterial() : Material("bust_material") {}
+
+    void init(GLint _diffuse_map, GLint _roughness_map, GLint _metallic_map, GLint _normal_map, GLint _ao_map) {
+        has_diffuse_map = true;
+        diffuse_map = _diffuse_map;
+
+        has_roughness_map = true;
+        roughness_map = _roughness_map;
+
+        has_metallic_map = true;
+        metallic_map = _metallic_map;
+
+        has_normal_map = true;
+        normal_map = _normal_map;
+
+        has_ao_map = false; //test
+        ao_map = _ao_map;
+        ao_factor = 0.05f;
+    }
+    void set_shader_uniforms(GLuint shader_program) const override {
+        Material::set_shader_uniforms(shader_program);
+    }
+};
+
+class LampMaterial : public Material {
+public:
+    LampMaterial() : Material("lamp_material") {}
+
+    void init(GLint _diffuse_map, GLint _roughness_map, GLint _metallic_map, GLint _normal_map, GLint _ao_map) {
         /*
-        glUniform1i(glGetUniformLocation(shader_program, "material.diffuse_map"), 1);
-        glUniform1i(glGetUniformLocation(shader_program, "material.has_diffuse_map"), 1);
-        glUniform1i(glGetUniformLocation(shader_program, "material.has_normal_map"), 0);
-        glUniform1f(glGetUniformLocation(shader_program, "material.metallic"), 0.75);
-        glUniform1i(glGetUniformLocation(shader_program, "material.has_metallic_map"), 0);
-        glUniform1f(glGetUniformLocation(shader_program, "material.roughness"), 0.5f);
-        glUniform1i(glGetUniformLocation(shader_program, "material.has_roughness_map"), 0);
-        glUniform1f(glGetUniformLocation(shader_program, "material.ao"), 0.05f);*/
+        diffuse_factor = glm::vec3(1.f, 1.f, 1.f);
+        roughness_factor = 0.5f;
+        metallic_factor = 1.f;
+        ao_factor = 0.05f;
+        */
+        has_heightmap = false;
+
+        has_diffuse_map = true;
+        diffuse_map = _diffuse_map;
+
+        has_roughness_map = true;
+        roughness_map = _roughness_map;
+
+        has_metallic_map = true;
+        metallic_map = _metallic_map;
+
+        has_normal_map = true;
+        normal_map = _normal_map;
+
+        has_ao_map = true;
+        ao_map = _ao_map;
+        ao_factor = 0.05f;
+    }
+    void set_shader_uniforms(GLuint shader_program) const override {
+        Material::set_shader_uniforms(shader_program);
     }
 };
