934d8d73a9406cc8dd86e973dc697db889ef3cb4
Davide Fantasia <davide.fantasia04@gmail.com>
2024-08-10 14:19:36 +0200

    final commit

--- common/frame_buffer_object.h
+++ common/frame_buffer_object.h
@@ -26,7 +26,6 @@
 			}
 		}
 
-
 		void create(int w_, int h_,bool _use_texture_for_depth = false)
 		{
 			if ((w == w_) && (h == h_)&& _use_texture_for_depth== use_texture_for_depth)
@@ -36,7 +35,7 @@
 			glGenFramebuffers(1, &this->id_fbo);
 			glBindFramebuffer(GL_FRAMEBUFFER, this->id_fbo);
 
-			/* texture for color attachment*/
+			//texture for color attachment
 			glGenTextures(1, &this->id_tex);
 			glBindTexture(GL_TEXTURE_2D, this->id_tex);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -45,10 +44,11 @@
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+			//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, w, h, 0, GL_RGB, GL_FLOAT, NULL);
 
 			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->id_tex, 0);
 
-			/* texture for color attachment 1*/
+			// texture for color attachment 1
 			glGenTextures(1, &this->id_tex1);
 			glBindTexture(GL_TEXTURE_2D, this->id_tex1);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -57,6 +57,7 @@
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+			//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, w, h, 0, GL_RGB, GL_FLOAT, NULL);
 
 			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, this->id_tex1, 0);
 
@@ -64,7 +65,7 @@
 			check_gl_errors(__LINE__, __FILE__, true);
 			use_texture_for_depth = _use_texture_for_depth;
 			if (_use_texture_for_depth) {
-				/* texture for depth  attachment*/
+				// texture for depth  attachment
 				glGenTextures(1, &this->id_depth);
 				glBindTexture(GL_TEXTURE_2D, this->id_depth);
 
@@ -79,17 +80,20 @@
 				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, this->id_depth, 0);
 			}
 			else
-			{	/* render buffer for depth  attachment*/
+			{	// render buffer for depth  attachment
 				glGenRenderbuffers(1, &this->id_depth);
 				glBindRenderbuffer(GL_RENDERBUFFER, this->id_depth);
-				//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, w, h);
-				glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, w, h);
+				glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, w, h);
 				glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, this->id_depth);
 			}
+
+			unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+			glDrawBuffers(2, attachments);
+
 			check_gl_errors(__LINE__, __FILE__, true);
 			int status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 			check(status);
-			std::this_thread::sleep_for(std::chrono::milliseconds(6000));
+			//std::this_thread::sleep_for(std::chrono::milliseconds(6000));
 			glBindFramebuffer (GL_FRAMEBUFFER, 0);
 		}
 
